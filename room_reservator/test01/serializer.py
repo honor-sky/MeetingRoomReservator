9ef69c94cbacac168b36c6b0d5c83ccd4716043f
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-11-21 01:56:39 +0900

    완성베리
    1. 프론트 목록 출력 및 예약 확정 팝업 구현(김준영)
    2. DB에 예약 정보 저장 및 기타 오류 수정(김경민)

--- room_reservator/test01/serializer.py
+++ room_reservator/test01/serializer.py
@@ -4,11 +4,16 @@ from .models import Roomreservation, Meetingroom
 class TestDataSerializer(ModelSerializer):
     class Meta:
         model = Roomreservation
-        #model2 = Meetingroom
         fields = '__all__'
 
 class TestDataSerializer2(ModelSerializer):
     class Meta:
         model = Meetingroom
         fields = '__all__'
+
+class SaveReservationSerializer(ModelSerializer):
+    class Meta:
+        model = Roomreservation
+        fields = '__all__'
+
 
--- room_reservator/test01/views.py
+++ room_reservator/test01/views.py
@@ -1,30 +1,20 @@
-from datetime import datetime
 
-from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Roomreservation, Meetingroom
-from .serializer import TestDataSerializer, TestDataSerializer2
+from .models import Roomreservation, Meetingroom, Employee
 
 
+@api_view(['GET','POST'])
 def index(request):
-    return render(request,"index.html")
 
-
-@api_view(['GET'])
-def index(request): #찾고자 하는 회의실ID 가져옴
     avaliableroom=[]
-    print(request.GET)
 
-    if(bool(request.GET)): #프론트에서 전달하는 값이 있을때만 DB 에서 추출
+    if(request.method == 'GET' and bool(request.GET)): #프론트에서 전달하는 값이 있을때만 DB 에서 추출 #bool(request.GET)
         isbeam = 0
         if (request.GET.get("vim_check") == "on"):
             isbeam = 1
         queryset = Meetingroom.objects.filter(meetingroomcapacity__gt=request.GET['count'],
                                               isbeamprojector=isbeam).values()  # .values_list('meetingroomid', flat=True)
-
         for i in range(len(queryset)):
             context_i = {}
             data = Roomreservation.objects.filter(reserveroom=queryset[i]['meetingroomid'],
@@ -34,6 +24,7 @@ def index(request): #찾고자 하는 회의실ID 가져옴
             if not data:
                 context_i['meetingroomid'] = queryset[i]['meetingroomid']
                 context_i['meetingroomname'] = queryset[i]['meetingroomname']
+                context_i['meetingroomloc'] = queryset[i]['meetingroomloc']
                 context_i['meetingroomcapacity'] = queryset[i]['meetingroomcapacity']
                 context_i['reservestarttime'] = '00:00:00'
                 context_i['reserveendtime'] = '24:00:00'
@@ -79,26 +70,31 @@ def index(request): #찾고자 하는 회의실ID 가져옴
                         context_j['reservestarttime'] = data[j].reserveendtime.strftime("%H:%M:%S")
                         context_j['reserveendtime'] = data[j + 1].reservestarttime.strftime("%H:%M:%S")
                         avaliableroom.append(context_j)
-                    print()
 
-        # serializer = TestDataSerializer(context, many=True)
-        # return Response(serializer.data)
-        print(avaliableroom)
+
+        queryset = Employee.objects.filter(employeeid=request.GET['reserverid']).values('name')
 
         context = {
-            'avaliableroom': avaliableroom
+            'avaliableroom': avaliableroom,
+            'reserver_name' : queryset[0]['name'],
+            'reserve_date' :  request.GET['date']
         }
         return render(request, "index.html", context=context)
 
-    return render(request, "index.html")
+    elif(request.method == 'POST' and bool(request.POST)):   # DB에 예약 정보 저장
+
+        meetingroom_data = Meetingroom.objects.filter(meetingroomname=request.POST['reserveroom'])
+        user_data = Employee.objects.filter(name=request.POST['name'])
 
 
+        reservation_data = Roomreservation()
+        reservation_data.reserveroom = meetingroom_data[0]
+        reservation_data.reserver = user_data[0]
+        reservation_data.reservedate = request.POST['date']
+        reservation_data.reservestarttime = request.POST['reservestarttime']
+        reservation_data.reserveendtime = request.POST['reserveendtime']
+        reservation_data.save()
+        return render(request, "index.html")
 
-@api_view(['POST'])
-def postMember(request):
-    reqData = request.data
-    serializer = TestDataSerializer(data=reqData)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:  # 단순 새로고침
+        return render(request, "index.html")
